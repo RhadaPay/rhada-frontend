9c202bf7e33e9e1a1a9e37206b784c73324af51e
Francesco Moca <francesco.moca@gmail.com>
2021-08-12 22:45:34 +0200

    Notes

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,28 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import "@babel/polyfill";
 import store from "./store";
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
+
 
 Vue.config.productionTip = false;
+
+
+Vue.use(VueApollo)
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'https://api.thegraph.com/subgraphs/name/superfluid-finance/superfluid-mumbai'
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 new Vue({
   router,
   vuetify,
   store,
+  apolloProvider,
   render: (h) => h(App),
 }).$mount("#app");
--- src/views/Payments.vue
+++ src/views/Payments.vue
@@ -18,29 +18,47 @@ import BaseCard from "@/components/BaseCard.vue";
 
 export default {
   components: { BaseCard },
-  props:{
+  props: {
     jobId: {
       required: false,
-      default: "0"
-    }
+      default: "0",
+    },
   },
-  data(){
-      return {
-          sf: null,
-          walletAddress: null,
-          recipientAddress: ""
-      }
+  data() {
+    return {
+      sf: null,
+      walletAddress: null,
+      recipientAddress: "",
+    };
   },
+/* TheGraph query on https://api.thegraph.com/subgraphs/name/superfluid-finance/superfluid-mumbai
 
+{
+    tokens(where: { symbol: "fDAIx" }) {
+        flows(
+            where: {
+                owner: "0x8f6112872339bf00e9985067301a0636957c6b81" cashflowsRecipient(jobId)
+           			recipient: "0x1df27a35703914574e690869b11643d7cd877235" finalApplicant(jobId)
+            }
+        ) {
+            sum
+            flowRate
+      			lastUpdate
+        }
+    }
+  }
+
+  IMPORTANT: both addresses must be lowercase and the formula to calculate the total received is: sum + flowRate(currentTimestamp-lastUpdate)
+*/
   async mounted() {
     this.walletAddress = await window.ethereum.request({
-        method: "eth_requestAccounts",
-        params: [
-          {
-            eth_accounts: {},
-          },
-        ],
-      });
+      method: "eth_requestAccounts",
+      params: [
+        {
+          eth_accounts: {},
+        },
+      ],
+    });
 
     this.sf = new SuperfluidSDK.Framework({
       ethers: new Web3Provider(window.ethereum),
@@ -59,10 +77,10 @@ export default {
       const details = await user.details();
       console.log(details);
 
-    await user.flow({
-           recipient: '', //TODO: should be calling TradeableCashflowWithAllowanceFactory.cashflowsRecipient(jobId)
-           flowRate: '' //TODO: should be calling TradeableCashflowWithAllowanceFactory.maxAllowedFlows(jobId)
-     });
+      await user.flow({
+        recipient: "", //TODO: should be calling TradeableCashflowWithAllowanceFactory.cashflowsRecipient(jobId)
+        flowRate: "", //TODO: should be calling TradeableCashflowWithAllowanceFactory.maxAllowedFlows(jobId)
+      });
     },
   },
 };
