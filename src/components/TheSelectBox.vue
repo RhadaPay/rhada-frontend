7ba3ee39f1313e6580f881e9782ba1e3878d3ec4
Jordan Imran <jordan.imran@predixa.ai>
2021-08-15 00:49:26 +0400

    added signing to apply grid

---
+++ src/components/TheSelectBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-icon>{{ complete ? 'mdi-check' : 'mdi-select' }}</v-icon>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    complete: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
--- src/views/Apply.vue
+++ src/views/Apply.vue
@@ -7,44 +7,103 @@
           <v-icon v-if="!loading">mdi-refresh</v-icon>
           <v-progress-circular v-else indeterminate />
         </v-btn>
+        <v-switch
+        class="mx-3"
+        :label="`${hideComplete ? 'Show' : 'Hide'} Completed`"
+        v-model="hideComplete"/>        
       </v-card-actions>
     </div>
     <v-data-table
       :loading="loading"
       v-model="selected"
       :headers="headers"
-      :items="tableData"
+      :items="tableData.filter(row => !row.finalSign || hideComplete)"
     >
-      <template v-slot:[`item.apply`]="props">
+      <template v-slot:[`item.applyForJob`]="props">
         <v-btn
           class="details"
+          depressed
+          :disabled="props.item.creator === currentAddress || !!props.item.finalApplicant"
+          icon
+          small
+          dark
+          color="primary"
+          @click="applyForJob(props.item)"
+          ><the-select-box :complete="!!props.item.finalApplicant"/></v-btn
+        >
+      </template>
+      <template v-slot:[`item.chooseApplicant`]="props">
+        <v-btn
+          class="details"
+          :disabled="props.item.creator !== currentAddress || !!props.item.finalApplicant"
           depressed
           icon
           small
           dark
           color="primary"
-          @click="onButtonClick(props.item)"
-          ><v-icon>mdi-view-list</v-icon></v-btn
+          @click="confirmApplicant(props.item)"
+          ><the-select-box :complete="!!props.item.finalApplicant"/></v-btn
         >
       </template>
-      <template v-slot:[`item.sign`]="props">
+      <template v-slot:[`item.initApplicantSign`]="props">
         <v-btn
           class="details"
+          :disabled="props.item.finalApplicant !== currentAddress || props.item.applicantSigned"
           depressed
           icon
           small
           dark
           color="primary"
-          @click="onButtonClick(props.item)"
-          ><v-icon>mdi-check</v-icon></v-btn
+          @click="applicantSign(props.item)"
+          ><the-select-box :complete="props.item.applicantSigned"/></v-btn
         >
       </template>
+      <template v-slot:[`item.initCreatorSign`]="props">
+        <v-btn
+          class="details"
+          :disabled="props.item.creator !== currentAddress || props.item.creatorSigned"
+          depressed
+          icon
+          small
+          dark
+          color="primary"
+          @click="creatorSign(props.item)"
+          ><the-select-box :complete="props.item.creatorSigned"/></v-btn
+        >
+      </template> 
+      <template v-slot:[`item.submitWork`]="props">
+        <v-btn
+          class="details"
+          :disabled="props.item.finalApplicant !== currentAddress || props.item.workSubmitted"
+          depressed
+          icon
+          small
+          dark
+          color="primary"
+          @click="submitWork(props.item)"
+          ><the-select-box :complete="props.item.workSubmitted"/></v-btn
+        >
+      </template>
+      <template v-slot:[`item.finalSign`]="props">
+        <v-btn
+          class="details"
+          :disabled="props.item.creator !== currentAddress || props.item.finalSign"
+          depressed
+          icon
+          small
+          dark
+          color="primary"
+          @click="finalSign(props.item)"
+          ><the-select-box :complete="props.item.finalSign"/></v-btn
+        >
+      </template>                               
     </v-data-table>
   </base-card>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import BaseCard from "@/components/BaseCard.vue";
+import TheSelectBox from "@/components/TheSelectBox.vue";
 import { paymentFactory } from "@/plugins/ethers";
 
 interface TableData {
@@ -55,25 +114,46 @@ interface TableData {
   eventStream: string;
   percentage: number;
   downPayment: number;
+  creator: string;
+  finalApplicant: string;
+  applicantSigned: boolean;
+  creatorSigned: boolean;
+  workSubmitted: boolean;
+  finalSign: boolean;
 }
 
 interface TableHeaders {
   text: string;
-  value: keyof TableData | "apply" | "sign";
+  value: keyof TableData | ApplicationStages
 }
+
+type ApplicationStages = "applyForJob"
+  | "chooseApplicant"
+  | "initApplicantSign"
+  | "initCreatorSign"
+  | "submitWork"
+  | "finalSign"
+  ;
 
 export default Vue.extend({
   components: {
     BaseCard,
+    TheSelectBox
   },
   data: () => ({
+    hideComplete: true,
     loading: false,
+    currentAddress: '',
     selected: [],
     tableData: [] as Array<TableData>,
   }),
   computed: {
     headers(): TableHeaders[] {
       return [
+        {
+          text: "Job ID",
+          value: 'id'
+        },
         {
           text: "Description",
           value: "descriptor",
@@ -96,21 +176,70 @@ export default Vue.extend({
         },
         {
           text: "Apply",
-          value: "apply",
+          value: "applyForJob",
         },
         {
-          text: "Sign",
-          value: "sign",
+          text: "Confirm Applicant",
+          value: "chooseApplicant",
         },
+        {
+          text: "Applicant Started",
+          value: "initApplicantSign",
+        },
+        {
+          text: "Terms Agreed",
+          value: "initCreatorSign",
+        },
+        {
+          text: "Work Submitted",
+          value: "submitWork",
+        },
+        {
+          text: "Final Sign",
+          value: "finalSign",
+        },                                
       ];
     },
   },
   methods: {
-    onButtonClick(item: TableData): void {
-      console.log("Take me to the apply for job screen");
-      this.$router.push({ name: "ApplyComplete" });
+    async applyForJob(item: TableData): Promise<void> {
+      this.loading = true;
+      await paymentFactory.applyForJob(item.id);
+      this.loading = false;
+    },
+    async confirmApplicant(item: TableData): Promise<void> {
+      this.loading = true;
+      const applicant = await paymentFactory.jobToApplicants(item.id, 0);
+      await paymentFactory.chooseApplicant(applicant, item.id);
+      this.loading = false;
+    },
+    async applicantSign(item: TableData): Promise<void> {
+      this.loading = true;
+      await paymentFactory.initApplicantSign(item.id);
+      this.loading = false;
+    },
+    async creatorSign(item: TableData): Promise<void> {
+      this.loading = true;
+      console.log(item);
+      await paymentFactory.initCreatorSign(item.id, {
+        value: 1
+      });
+      this.loading = false;
+    },
+    async submitWork(item: TableData): Promise<void> {
+      this.loading = true;
+      await paymentFactory.submitWork(item.id, 'NOT PROVIDED');
+      this.loading = false;
+    },
+    async finalSign(item: TableData): Promise<void> {
+      this.loading = true;
+      const allowedFlow = 289351851851852;
+      const maxAllowedFlow = 578703703703704
+      await paymentFactory.finalSign(true, item.id, allowedFlow, maxAllowedFlow);
+      this.loading = false;
     },
     async getTableData(): Promise<void> {
+      this.loading = true;
       const jobs = await paymentFactory.getJobs();
       const rows: TableData[] = [];
       jobs.forEach(async (job, jobId) => {
@@ -124,8 +253,14 @@ export default Vue.extend({
           eventStream: await paymentFactory.eventStreams(
             job.eventStreamId.toNumber()
           ),
+          creator: job.creator,
+          finalApplicant: await paymentFactory.finalApplicant(jobId),
+          applicantSigned: job.applicantSigned,
+          creatorSigned: job.creatorSigned,
+          workSubmitted: job.workSubmitted,
           percentage: job.percentage,
           downPayment: job.amount.toNumber(),
+          finalSign: job.state === 3
         };
         rows.push(tableRow);
       });
@@ -139,6 +274,7 @@ export default Vue.extend({
   },
   async mounted() {
     this.refresh();
+    this.currentAddress = await paymentFactory.signer.getAddress();
   },
 });
 </script>
